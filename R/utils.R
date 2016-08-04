ae5e4ece81b1c0bd785971533e997250f6870448
Julio Trecenti <julio.trecenti@gmail.com>
2016-08-04 13:37:06 -0500

    unicas funcoes que sobraram

---
+++ R/utils.R
@@ -0,0 +1,90 @@
+#' Vetorizando scrapers
+#'
+#' Vetoriza um scraper (função) para um vetor de itens
+#'
+#' @param fun função a ser aplicada em cada arquivo.
+#' @param itens character vector dos caminhos de arquivos a serem transformados.
+#' @param ... outros parâmetros a serem passados para \code{fun}
+#' @param verbose se \code{TRUE} (default), mostra o item com probabilidade p.
+#' @param p probabilidade de imprimir mensagem.
+#' 
+#' @export
+dvec <- function(fun, itens, ..., verbose = TRUE, p = .05) {
+  f <- dplyr::failwith(data_frame(result = 'erro'), fun)
+  tibble::data_frame(item = itens) %>%
+    dplyr::distinct(item) %>%
+    dplyr::group_by(item) %>%
+    dplyr::do({
+      if (runif(1) < p && verbose) print(.$item)
+      d <- f(.$item, ...)
+      if (tibble::has_name(d, 'result')) d$result <- 'OK'
+      d
+    }) %>%
+    dplyr::ungroup()
+}
+
+
+#' Remove acentos.
+#' 
+#' Remove os acentos da string.
+#' 
+#' @param x texto ou vetor de textos.
+#' 
+#' @export
+rm_accent <- function(x) {
+  if (.Platform$OS.type == 'unix') {
+    gsub("`", "", iconv(x, to="ASCII//TRANSLIT"))
+  } else {
+    if (!is.character(x)) x <- as.character(x)
+    symbols <- c(
+      acute = "áéíóúÁÉÍÓÚýÝ",
+      grave = "àèìòùÀÈÌÒÙ",
+      circunflex = "âêîôûÂÊÎÔÛ",
+      tilde = "ãõÃÕñÑ",
+      umlaut = "äëïöüÄËÏÖÜÿ",
+      cedil = "çÇ"
+    )
+    nudeSymbols <- c(
+      acute = "aeiouAEIOUyY",
+      grave = "aeiouAEIOU",
+      circunflex = "aeiouAEIOU",
+      tilde = "aoAOnN",
+      umlaut = "aeiouAEIOUy",
+      cedil = "cC"
+    )
+    accentTypes <- c("´","`","^","~","¨","ç")
+    return(chartr(paste(symbols, collapse = ""), paste(nudeSymbols, collapse = ""), x)) 
+  }
+}
+
+.ls.objects <- function (pos = 1, pattern, order.by,
+                         decreasing=FALSE, head=FALSE, n=5) {
+  napply <- function(names, fn) sapply(names, function(x)
+    fn(get(x, pos = pos)))
+  names <- ls(pos = pos, pattern = pattern)
+  obj.class <- napply(names, function(x) as.character(class(x))[1])
+  obj.mode <- napply(names, mode)
+  obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+  obj.size <- napply(names, object.size)
+  obj.dim <- t(napply(names, function(x)
+    as.numeric(dim(x))[1:2]))
+  vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+  obj.dim[vec, 1] <- napply(names, length)[vec]
+  out <- data.frame(obj.type, obj.size, obj.dim)
+  names(out) <- c("Type", "Size", "Rows", "Columns")
+  if (!missing(order.by))
+    out <- out[order(out[[order.by]], decreasing=decreasing), ]
+  if (head)
+    out <- head(out, n)
+  out
+}
+
+#' Lista objetos.
+#' 
+#' @param ... outros parâmetros passados para \code{.ls.objects}.
+#' @param n apenas os n com maior tamanho.
+#' 
+#' @export
+lsos <- function(..., n=10) {
+  .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+}
