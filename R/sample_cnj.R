a3c6772d8616382c18ecb1f553848799c97ae5ef
azeloc <fe_poliano@yahoo.com.br>
2016-12-22 18:26:58 -0200

    fix wrong reference to abjur/procMiner package

--- R/sample_cnj.R
+++ R/sample_cnj.R
@@ -9,7 +9,7 @@ sample_cnj <- function(n, foros, anos, orgao, tr, first_dig = '0'){
     as.character %>%
     stringr::str_sub(3,8) %>%
     dplyr::data_frame() %>%
-    dplyr::mutate(n_processo = procMiner::calcula_dig(sprintf('%s%s%s%s%s%s',
+    dplyr::mutate(n_processo = abjutils::calc_dig(sprintf('%s%s%s%s%s%s',
                                                               first_dig,
                                                               .,
                                                               sample(rep(anos,2),n,replace = T),
