8208dad6430eab116cd31b8d246eaac8b69788ed
azeloc <fe_poliano@yahoo.com.br>
2016-12-22 18:16:30 -0200

    add sample_cnj function

---
+++ R/sample_cnj.R
@@ -0,0 +1,21 @@
+#' Sample brazillian process codes
+#'
+#'
+#'
+#'@export
+sample_cnj <- function(n, foros, anos, orgao, tr, first_dig = '0'){
+
+  runif(n) %>%
+    as.character %>%
+    stringr::str_sub(3,8) %>%
+    dplyr::data_frame() %>%
+    dplyr::mutate(n_processo = procMiner::calcula_dig(sprintf('%s%s%s%s%s%s',
+                                                              first_dig,
+                                                              .,
+                                                              sample(rep(anos,2),n,replace = T),
+                                                              orgao,
+                                                              tr,
+                                                              sample(rep(foros,2),n,replace = T)
+    ), monta = T)) %>%
+    dplyr::select(n_processo)
+}
