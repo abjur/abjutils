c571d06ddf93517c7482cf2b9af4db7cc233a91f
Fernando Corrêa <fptcorrea@gmail.com>
2017-09-01 23:18:32 -0300

    table_utils

---
+++ R/table_utils.R
@@ -0,0 +1,28 @@
+prettify_number <- function(d, number = T, percent = T,  ...){
+  
+  if(percent){
+    query <- '{fmt_p(x/sum(x))}'
+  } 
+  
+  if(number) {
+    query <- sprintf('{fmt(x)} (%s)', glue_query)
+  }
+  
+  d %>% 
+    select(...) %>% 
+    mutate_if(is.numeric, .funs = function(x){glue(query)}) %>% 
+    as_data_frame()
+}
+
+fmt <- function(x) {
+  format(x, big.mark = '.', small.mark = ',',
+         decimal.mark = ',',
+         scientific = FALSE, trim = TRUE)
+}
+
+fmt_p <- function(x) {
+  if (length(x) == 0) return(character())
+  x <- plyr::round_any(x, scales:::precision(x) / 100)
+  x <- fmt(x * 100)
+  paste0(x, "%")
+}
