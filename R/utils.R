6e4aeaed09ea152e176f7f4bdeee8d5afb4251c8
ctlente <ctlente@gmail.com>
2018-05-30 18:14:58 -0300

    Attempt at fixing rm_accent on windows

--- R/utils.R
+++ R/utils.R
@@ -232,7 +232,7 @@ rm_accent <- function(x) {
   if (.Platform$OS.type == 'unix') {
     stringr::str_replace_all(iconv(x, to = "ASCII//TRANSLIT"), "[`'\"^~]", "")
   } else {
-    gsub("`", "", iconv(x, from = 'latin1', to="ASCII//TRANSLIT"))
+    stringi::stri_trans_general(x, "Latin-ASCII")
   }
 }
 
