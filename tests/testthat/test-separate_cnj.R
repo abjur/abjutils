a676ff83da4005a5818f15998746e25c776f5cf0
C. Lente <7017340+clente@users.noreply.github.com>
2022-01-31 16:37:33 -0300

    Translate tests

--- tests/testthat/test-separate_cnj.R
+++ tests/testthat/test-separate_cnj.R
@@ -1,7 +1,7 @@
 
-context("Funcoes de separate")
+context("separate")
 
-test_that("separate_cnj funciona sem warning", {
+test_that("separate_cnj works", {
   library(magrittr)
   da <- data.frame(
     a = "1000315-72.2016.8.26.0156"
@@ -10,6 +10,6 @@ test_that("separate_cnj funciona sem warning", {
 
   testthat::expect_warning(resp <- separate_cnj(da, a), regexp = NA)
 
-  testthat::expect_equal(class(resp), c("tbl_df", "tbl", "data.frame"))
+  testthat::expect_equal(class(resp), "data.frame")
   testthat::expect_equal(dim(resp), c(1, 6))
 })
